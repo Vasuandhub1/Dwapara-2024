6197a24d95097f7ce1b091091cdc91a984a6d1a8
prasanna <prasannasamadhiya02@gmail.com>
2024-02-16 20:54:42 -0800

    Navbar and about us added

---
+++ src/components/aboutUs/about.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import image from "./3821954.webp"
+
+function About() {
+    return (
+        <div className="w-full h-full bg-[#043873] text-white">
+               <div className="  text-9xl font-bold text-center ">About Us</div>
+               <div className="flex flex-col">
+                   <div className="text-2xl text-center m-20"><b> &quot; The blessing of Knowledge empoweres like fury of a tiger &quot;</b></div>
+                    <div className="items-center justify-center">
+                    <div className="flex justify-center items-center">
+                        <img src={image} alt='image' className="text-center" />
+                    </div>
+                    <div className="p-12 text-2xl">We here at sahapathi know that in this digital edge ,the student requires 
+                    more than just a big road map to follow which may span over many years.The 
+                    student 's need to follow through his predecided goal must be met with 
+                    a product catering to the goal 's exclusive content.so,we have developed a 
+                    solution which takes the time that the goal is to be acheived and the daily
+                    hours the student thinks he can give to the goals.</div>
+                    </div>
+               </div>
+        </div>
+    )
+}
+
+export default About
